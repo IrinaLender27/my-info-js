60f520b8a8234bc0dce1629d714f2860d0ff2d01
IrinaLender27 <irina.lender27@gmail.com>
2023-12-03 00:42:34 +0200

    update

---
+++ src/js/10-1-api.js
@@ -0,0 +1,16 @@
+// 1. Postman - використовується для створення запиту на backend без написання коду
+// 2. Корисна інформація та АРІ для тесту
+// https://www.weatherapi.com/
+// https://uk.wikipedia.org/wiki/SOAP
+// https://uk.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%BA%D0%BE%D0%B4%D1%96%D0%B2_%D1%81%D1%82%D0%B0%D0%BD%D1%83_HTTP#4xx_%D0%9A%D0%BB%D1%96%D1%94%D0%BD%D1%82%D1%81%D1%8C%D0%BA%D0%B0_%D0%BF%D0%BE%D0%BC%D0%B8%D0%BB%D0%BA%D0%B0
+// https://www.weatherapi.com/docs/
+// https://api.privatbank.ua/#p24/exchange
+// https://handlebarsjs.com/guide/#what-is-handlebars
+// https://swapi.dev/
+// https://www.udemy.com/
+
+// 3. Синтаксис
+// ?- вказує на старт параметрів запиту.
+// пара ім'я=значення.
+// & використовується для зазначення смислового «І», розділяючи параметри в рядку запиту.
+// Приклад :const url = "https://jsonplaceholder.typicode.com/users?_limit=7&_sort=name";
