e2b3d68a267c7e4bc80d3625485938c0dba88b1e
IrinaLender27 <irina.lender27@gmail.com>
2023-12-03 21:30:58 +0200

    api

--- src/js/10-1-api.js
+++ src/js/10-1-api.js
@@ -14,3 +14,56 @@
 // пара ім'я=значення.
 // & використовується для зазначення смислового «І», розділяючи параметри в рядку запиту.
 // Приклад :const url = "https://jsonplaceholder.typicode.com/users?_limit=7&_sort=name";
+
+// 4. Створення прогноза погоди
+// - за допомогою Poatman створили запит http://api.weatherapi.com/v1/forecast.json?key=453cf0534c434bc7ba1221606230212 &q=Paris&days=5
+
+const search = document.querySelector('.js-search');
+const list = document.querySelector('.js-list');
+// Додаємо слухача на форму
+search.addEventListener('submit', onSearch);
+function onSearch(event) {
+  event.preventDefault();
+  // Робимо деструктуризацію,
+  const { query, days } = event.currentTarget.elements;
+  //   Функція отримання значень
+  getWeather(query.value, days.value)
+    .then(data => (list.innerHTML = createMarkup(data.forecast.forecastday)))
+    .catch(err => console.log(err));
+}
+// Запит на backend
+function getWeather(city, days) {
+  // http://api.weatherapi.com/v1/forecast.json?key=453cf0534c434bc7ba1221606230212 &q=Paris&days=5
+  const BASE_URL = 'http://api.weatherapi.com/v1';
+  const API_KEY = '47cce444510845a3b5890337232811';
+  return fetch(
+    `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}`
+  ).then(response => {
+    // перевірка статусу ok : true  aбо false
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
+// Створення розмітки
+function createMarkup(arr) {
+  // Перебираємо отриманий масив
+  return arr
+    .map(
+      ({
+        date,
+        day: {
+          avgtemp_c,
+          condition: { icon, text },
+        },
+      }) =>
+        `<li>
+        <img src="${icon}" alt="${text}" />
+        <p>${text}</p>
+        <h2>${date}</h2>
+        <h3>${avgtemp_c}</h3>
+      </li>`
+    )
+    .join('');
+}
